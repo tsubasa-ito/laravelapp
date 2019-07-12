9f4d081af6b19af97e2cfe1c967a5fe7553bb11b
伊藤翼 <tsubasa8770@gmail.com>
2019-07-12 10:35:50 +0900

    4-2 first

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -41,9 +41,25 @@
 @endsection
 
 @section('content')
-    <p>this is content</p>
-    <p>This is <middleware>google.com</middleware> link!</p>
-    <p>This is <middleware>yahoo.co.jp</middleware> link!</p>
+    <p>{{$msg}}</p>
+    @if (count($errors)>0)
+    <div>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    <table>
+        <form action="/hello" method="post">
+        {{ csrf_field() }}
+        <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
+        <tr><th>mail: </th><td><input type="text" name="mail" value="{{old('mail')}}"></td></tr>
+        <tr><th>age: </th><td><input type="text" name="age" value="{{old('age')}}"></td></tr>
+        <tr><th>send: </th><td><input type="submit" name="send" value="send"></td></tr>
+        </form>
+    </table>
 @endsection
 
 @section('footer')
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-//use App\Http\Middleware\HelloMiddleware;
+use App\Http\Middleware\HelloMiddleware;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,7 +16,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('hello', 'HelloController@index')
-    ->middleware('hello');
-    // ->middleware(HelloMiddleware::class);
+    // ->middleware('hello');
+    ->middleware(HelloMiddleware::class);
 
 Route::post('hello', 'HelloController@post');
