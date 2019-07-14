d94474f4bf40a7d298041206dfcb40725c9bbc50
伊藤翼 <tsubasa8770@gmail.com>
2019-07-15 01:31:30 +0900

    chapter 4-3 validator

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,35 +1,3 @@
-{{-- <html>
-<head>
-<title>Blade/Index</title>
-<style>
-    body {
-    font-size: 16pt;
-    color: #999;
-    }
-    h1 {
-        font-size: 50pt;
-        text-align: right;
-        color: #f6f6f6;
-        margin: -20px 0px -30px 0px;
-        letter-spacing: -4pt;
-    }
-</style>
-</head>
-<body>
-    <h1>Blade/Index</h1>
-    @isset ($msg)
-    <p>こんにちは。{{ $msg }}さん。</p>
-    @else
-    <p>何か描いてください。</p>
-    @endisset
-
-    <form method= "POST" action="/hello">
-        {{ csrf_field() }}
-        <input type="text" name="msg">
-        <input type="submit">
-    </form>
-</body>
-</html> --}}
 
 @extends('layout.helloapp')
 
